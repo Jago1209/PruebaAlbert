966479c85d1ad546adb065d0c70825f278a514fe
jago1209 <gamobaj@gmail.com>
2021-09-21 00:02:55 -0500

    first commit

--- src/components/mapComponent/layers/control-layers.js
+++ src/components/mapComponent/layers/control-layers.js
@@ -1,16 +1,16 @@
 import L from 'leaflet'
 
 //BASEMAPS 
-export var standard_osm = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '©OpenStreetMap, ©Standard',minZoom: 0, maxZoom: 30});
-export var standard_osm_mm = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '©OpenStreetMap, ©Standard',minZoom: 0, maxZoom: 30});
-export var carto_light = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {attribution: '©OpenStreetMap, ©CartoDB',subdomains: 'abcd',maxZoom: 30});
+export var standard_osm = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '©OpenStreetMap, ©Standard',minZoom: 0, maxZoom: 19});
+export var standard_osm_mm = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '©OpenStreetMap, ©Standard',minZoom: 0, maxZoom: 19});
+export var carto_light = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {attribution: '©OpenStreetMap, ©CartoDB',subdomains: 'abcd',maxZoom: 19});
 
 export var r_perimetro = L.tileLayer.wms("http://34.132.27.64:8080/geoserver/yopal/wms?", {
     layers: 'yopal:r_perimetro',
     format: 'image/png',
     transparent: true,
     minZoom: 0, 
-    maxZoom: 30
+    maxZoom: 19
     //attribution: "Weather data © 2012 IEM Nexrad"
 });
 
@@ -19,6 +19,6 @@ export var u_terreno = L.tileLayer.wms("http://34.132.27.64:8080/geoserver/yopal
     format: 'image/png',
     transparent: true,
     minZoom: 0,
-    maxZoom: 30
+    maxZoom: 19
     //attribution: "Weather data © 2012 IEM Nexrad"
 });
--- src/components/mapComponent/map-control.js
+++ src/components/mapComponent/map-control.js
@@ -1,15 +1,17 @@
 import $, { getJSON } from "jquery";
 import 'leaflet/dist/leaflet.css';
 import './map.scss';
+
+import './controls/dist/leaflet-control-boxzoom';
+import './controls/dist/leaflet-control-boxzoom.css';
 import 'leaflet-minimap/dist/Control.MiniMap.min.css';
 
 const L =require ('leaflet');
 
-import {standard_osm} from './layers/control-layers'
-import {carto_light} from './layers/control-layers'
+import {standard_osm} from './layers/control-layers';
 import {r_perimetro} from './layers/control-layers';
 import {u_terreno} from './layers/control-layers';
-import {AwesomeMarkersIcon} from './controls/icons/famIcon'
+import {AwesomeMarkersIcon} from './controls/icons/famIcon';
 
 export var codigo = "";
 export var shape_area = "";
@@ -21,54 +23,74 @@ export var map = L.map('map', {
     layers: [standard_osm, r_perimetro, u_terreno]
 });
 
-L.control.zoom({position: 'topright'}).addTo(map);
+//BoxZoom
+new L.Control.boxzoom({ position:'topleft' }).addTo(map);
 
-// scale control
+//scale control
 new L.control.scale({imperial: false}).addTo(map)
 
 //marcador
 L.marker([5.3357, -72.3942], {icon: AwesomeMarkersIcon('glyphicon', 'flag','red')}).addTo(map);
 
 //info punto
-
 map.on('click', function(e) {        
     var popLocation= e.latlng;
-    
-
-    var este = getInfo();
-
-    if(codigo == "" ||  shape_area ==""){
-    var popup = L.popup()
-    .setLatLng(popLocation)
-    .setContent('<p>No se tiene información de esta área</p>')
-    .openOn(map);
-
-    }else{
-    var popup = L.popup()
-    .setLatLng(popLocation)
-    .setContent('<p>Codigo: <br/>' + codigo + '</p><br/> <p>Area: <br/>' + shape_area + '</p>')
-    .openOn(map); 
-    codigo = "";
-    shape_area = "";
-    }
-
-           
+    //llamamos la funcion que envia el punto en el que se hizo click
+    var este = getInfo(popLocation); 
 });
 
+//capturamos punto de origen del click
+var x = 0;
+var y = 0; 
+document.addEventListener('click', onMouseUpdate, true);
+function onMouseUpdate(e) {
+  x = e.pageX;
+  y = e.pageY;
+}
+
+//capturamos el tamaño de la ventana
+var height = window.innerHeight;
+var width = window.innerWidth;
 
 
- function getInfo(){
+
+//consulta a geoserver
+ function getInfo(popLocation){
     var location = map.getBounds().toBBoxString();
+    var length;
+
+    //url y parametros de consulta
     var settings = {
-        "url": "http://34.132.27.64:8080/geoserver/yopal/wms?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetFeatureInfo&FORMAT=image/Fpng&TRANSPARENT=true&QUERY_LAYERS=yopal:u_terreno&LAYERS=yopal:u_terreno&exceptions=application/Fvnd.ogc.se_inimage&INFO_FORMAT=application/json&FEATURE_COUNT=1&X=15&Y=15&SRS=EPSG:4326&WIDTH=31&HEIGHT=31&typeName=namespace:numero_sub&BBOX="+ location,
+        "url": "http://34.132.27.64:8080/geoserver/yopal/wms?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetFeatureInfo&FORMAT=image/Fpng&TRANSPARENT=true&QUERY_LAYERS=yopal:u_terreno&LAYERS=yopal:u_terreno&exceptions=application/Fvnd.ogc.se_inimage&INFO_FORMAT=application/json&FEATURE_COUNT=1&X=" + x + "&Y=" + y + "&SRS=EPSG:4326&WIDTH=" + width + "&HEIGHT=" + height + "&typeName=namespace:numero_sub&BBOX="+ location,
         "method": "GET",
         "timeout": 0,
       };
       
+      //ejecuto consulta
       $.ajax(settings).done(function (response) {
-        codigo = response.features[0].properties.codigo;
-        shape_area = response.features[0].properties.shape_area;
+        //capturo longitud de la respuesta 
+        length = response.features.length;
+        //confirmo si la consulta genero respuesta o no
+        if(length == 0){
+
+          var popup = L.popup()
+          .setLatLng(popLocation)
+          .setContent('<p>No se tiene información de esta área</p>')
+          .openOn(map);
+
+            }else{
+            codigo = response.features[0].properties.codigo;
+            shape_area = response.features[0].properties.shape_area;
+            
+            var popup = L.popup()
+            .setLatLng(popLocation)
+            .setContent('<p>Codigo: <br/>' + codigo + '</p><br/> <p>Area: <br/>' + shape_area + '</p>')
+            .openOn(map); 
+
+            codigo = "";
+            shape_area = "";
+          }
       });
 
-      return codigo, shape_area;
 }
+
